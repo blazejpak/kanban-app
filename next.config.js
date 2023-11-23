531405a1e853c4e97353aa6fc935ca7f8d1731b4
blazejpak <blazej.pak@gmail.com>
2023-11-23 18:08:30 +0100

    added responsive for forms

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverActions: true,
-    serverComponentsExternalPackages: ["mongoose"],
-  },
-};
-
-module.exports = nextConfig;
--- store/provider.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import { Provider } from "react-redux";
-import { store } from "./store";
-
-export function ProvideStore({ children }: any) {
-  return <Provider store={store}>{children}</Provider>;
-}
